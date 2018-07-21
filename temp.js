d69c477482b3afa8835ce0a4065997d756934fd8
First529 <first_dejabhol@hotmail.com>
2018-07-21 19:42:04 +0700

    ssd

--- temp.js
+++ temp.js
@@ -1,23 +1,34 @@
 var y = 0
-$(function(){
-    setInterval(function(){
-        
-            $.ajax({
-                type: "GET",
-                url: "http://ecourse.cpe.ku.ac.th/exceed/api/fingerpalm-temp/view",
-                dataType: "text",
-                success: function (response) {
-                    console.log("from server : "+response)
-                    y = response
+var total = 0;
+var count = 0
+$(function () {
+    setInterval(function () {
+
+        $.ajax({
+            type: "GET",
+            url: "http://ecourse.cpe.ku.ac.th/exceed/api/fingerpalm-temp/view",
+            dataType: "text",
+            success: function (response) {
+                console.log("from server : " + response)
+                y = response
+                count++;
+                total += parseInt(y);
+                if (count % 10 == 0) {
+
+                    var average = total / 10;
+                    console.log("average " + average);
+                    $('#box').text(response)
+                    total = 0
                 }
-            });
-        },5000)
-    $('#on-button').on('click',function(){
+            }
+        });
+    }, 1000)
+    $('#on-button').on('click', function () {
         $.ajax({
             type: "POST",
             url: "http://ecourse.cpe.ku.ac.th/exceed/api/fingerpalm-temp/set",
             data: {
-                value:"20"
+                value: "20"
             },
             dataType: "json",
             success: function (response) {
@@ -28,55 +39,57 @@ $(function(){
 
 })
 
-    window.onload = function () {
+window.onload = function () {
 
     var raw_data = [];
 
     var dps = []; // dataPoints
     var chart = new CanvasJS.Chart("chartContainer", {
-        title :{
-            text: "Temperature chart (per 5 second)"
+        title: {
+            text: "Temperature chart (per second)"
         },
         axisY: {
             includeZero: false
-        },      
+        },
         data: [{
             type: "line",
             dataPoints: dps
         }]
     });
-    
+
+
     var xVal = 0;
-    var yVal = 100; 
+    var yVal = 0;
     var updateInterval = 1000;
     var dataLength = 20; // number of dataPoints visible at any point
-    
+
     var updateChart = function (count) {
-    
+
         count = count || 1;
-    
+
         dps.push({
             x: xVal,
             y: yVal
         })
-        xVal += 5;
+        xVal++;
         console.log(y)
         yVal = parseInt(y)
         raw_data.push(y)
         console.log(raw_data)
-    
+
+
         if (dps.length > dataLength) {
             dps.shift();
         }
-    
+
         chart.render();
     };
-    
+
     updateChart(dataLength);
-    setInterval(function(){updateChart()}, updateInterval);
-    
-    }
-    
-    
+    setInterval(function () { updateChart() }, updateInterval);
+
+}
+
+
 
 
